c56a53f107c3f8102a73916ffc464c61b668dd62
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2025-07-03 14:07:14 -0400

    Add basic benches

---
+++ benches/aggregation.rs
@@ -0,0 +1,153 @@
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use hlagg::protocol::{dl::DiscreteLog, Aggregation};
+use curve25519_dalek::RistrettoPoint;
+use rand_core::OsRng;
+use std::hint::black_box;
+
+mod common;
+use common::*;
+
+type G = RistrettoPoint;
+type Agg = DiscreteLog<G>;
+
+// TODO: Tweak
+const NUM_CLIENTS: [usize; 1] = [10000];
+const LENGTHS: [usize; 3] = [1, 5, 10];
+
+fn bench_setup(c: &mut Criterion) {
+    let mut group = c.benchmark_group("setup");
+    for num_clients in NUM_CLIENTS {
+        for length in LENGTHS {
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("{num_clients}_clients_{length}_inputs")),
+                &(num_clients, length),
+                |b, &(num_clients, length)| {
+                    b.iter(|| {
+                        black_box(Agg::setup(num_clients, length, &mut OsRng));
+                    });
+                }
+            );
+        }
+    }
+    group.finish();
+}
+
+fn bench_encode(c: &mut Criterion) {
+    let mut group = c.benchmark_group("encode");
+    for num_clients in NUM_CLIENTS {
+        for length in LENGTHS {
+            let (_params, _sk, cks) = Agg::setup(num_clients, length, &mut OsRng);
+            let input = random_binary_vec(length);
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("{num_clients}_clients_{length}_inputs")),
+                &(num_clients, length),
+                |b, _| {
+                    b.iter(|| {
+                        black_box(Agg::encode(&cks[0], &input, &mut OsRng).unwrap());
+                    });
+                }
+            );
+        }
+    }
+    group.finish();
+}
+
+fn bench_verify(c: &mut Criterion) {
+    let mut group = c.benchmark_group("verify_encodings");
+    group.sample_size(10);
+    for num_clients in NUM_CLIENTS {
+        for length in LENGTHS {
+            let (params, _sk, cks) = Agg::setup(num_clients, length, &mut OsRng);
+            let inputs = random_inputs(num_clients, length);
+            let encodings_and_proofs: Vec<_> = cks.iter().zip(inputs.iter())
+                .map(|(ck, input)| Agg::encode(ck, input, &mut OsRng).unwrap())
+                .collect();
+            let (encodings, proofs): (Vec<_>, Vec<_>) = encodings_and_proofs.into_iter().unzip();
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("{num_clients}_clients_{length}_inputs")),
+                &(num_clients, length),
+                |b, _| {
+                    b.iter(|| {
+                        black_box(Agg::verify_encodings(&params, None, &encodings, &proofs).unwrap());
+                    });
+                }
+            );
+        }
+    }
+    group.finish();
+}
+
+fn bench_aggregate(c: &mut Criterion) {
+    let mut group = c.benchmark_group("aggregate");
+    for num_clients in NUM_CLIENTS {
+        for length in LENGTHS {
+            let (params, _sk, cks) = Agg::setup(num_clients, length, &mut OsRng);
+            let inputs = random_inputs(num_clients, length);
+            let encodings: Vec<_> = cks.iter().zip(inputs.iter())
+                .map(|(ck, input)| Agg::encode(ck, input, &mut OsRng).unwrap().0)
+                .collect();
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("{num_clients}_clients_{length}_inputs")),
+                &(num_clients, length),
+                |b, _| {
+                    b.iter(|| {
+                        black_box(Agg::aggregate(&params, &encodings).unwrap());
+                    });
+                }
+            );
+        }
+    }
+    group.finish();
+}
+
+fn bench_decode(c: &mut Criterion) {
+    let mut group = c.benchmark_group("decode");
+    for num_clients in NUM_CLIENTS {
+        for length in LENGTHS {
+            let (params, sk, cks) = Agg::setup(num_clients, length, &mut OsRng);
+            let inputs = random_inputs(num_clients, length);
+            let encodings: Vec<_> = cks.iter().zip(inputs.iter())
+                .map(|(ck, input)| Agg::encode(ck, input, &mut OsRng).unwrap().0)
+                .collect();
+            let agg = Agg::aggregate(&params, &encodings).unwrap();
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("{num_clients}_clients_{length}_inputs")),
+                &(num_clients, length),
+                |b, _| {
+                    b.iter(|| {
+                        black_box(Agg::decode(&sk, agg.clone()).unwrap());
+                    });
+                }
+            );
+        }
+    }
+    group.finish();
+}
+
+fn bench_post_process(c: &mut Criterion) {
+    let mut group = c.benchmark_group("post_process");
+    for num_clients in NUM_CLIENTS {
+        for length in LENGTHS {
+            let (params, sk, cks) = Agg::setup(num_clients, length, &mut OsRng);
+            let inputs = random_inputs(num_clients, length);
+            let encodings: Vec<_> = cks.iter().zip(inputs.iter())
+                .map(|(ck, input)| Agg::encode(ck, input, &mut OsRng).unwrap().0)
+                .collect();
+            let agg = Agg::aggregate(&params, &encodings).unwrap();
+            let partial = Agg::decode(&sk, agg).unwrap();
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("{num_clients}_clients_{length}_inputs")),
+                &(num_clients, length),
+                |b, _| {
+                    b.iter(|| {
+                        black_box(Agg::post_process(&params, partial.clone()).unwrap());
+                    });
+                }
+            );
+        }
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_setup, bench_encode, bench_verify, bench_aggregate, bench_decode, bench_post_process);
+criterion_main!(benches); 
---
+++ benches/common.rs
@@ -0,0 +1,19 @@
+use rand_core::OsRng;
+use rand::Rng;
+use hlagg::protocol::serialization::ToBytes;
+
+/// Generate a random binary vector of the given length.
+pub fn random_binary_vec(len: usize) -> Vec<u32> {
+    let mut rng = OsRng;
+    (0..len).map(|_| if rng.gen_bool(0.5) { 1 } else { 0 }).collect()
+}
+
+/// Return the serialized size (in bytes) of an item implementing ToBytes.
+pub fn serialized_size<T: ToBytes>(item: &T) -> usize {
+    item.to_bytes().len()
+}
+
+/// Generate random binary input vectors for multiple clients.
+pub fn random_inputs(num_clients: usize, length: usize) -> Vec<Vec<u32>> {
+    (0..num_clients).map(|_| random_binary_vec(length)).collect()
+} 
